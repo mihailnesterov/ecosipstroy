191988a2c157d7c47a5d269a4e3ff2263965aa9b
mihailnesterov <mhause@mail.ru>
2022-05-08 04:42:52 +0700

    add: create_option_type_table

---
+++ migrations/m220507_212443_create_option_type_table.php
@@ -0,0 +1,78 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%option_type}}`.
+ */
+class m220507_212443_create_option_type_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $tableOptions = $this->getTableOptions();
+
+        $tableName = '{{%option_type}}';
+
+        $this->createTable($tableName, [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(255)->notNull()->unique(),
+            'description' => $this->text()->defaultValue(null),
+        ], $tableOptions);
+
+        $table = $this->getDb()->getSchema()->getTableSchema($tableName);
+        
+        if( isset($table) ) {
+
+            $this->addCommentOnTable($tableName, 'Таблица типов опций');
+
+            $comments = [
+                'id' => 'id типа',
+                'name' => 'Название типа',
+                'description' => 'Описание'
+            ];
+
+            foreach ($comments as $key => $value) {
+                if( !empty($table->getColumn($key)) ) {
+                    $this->addCommentOnColumn($tableName, $key, $value);
+                }
+            }
+
+            $this->addForeignKey(
+                'fk-option-type-id', 
+                'option', 
+                'type_id', 
+                'option_type', 
+                'id', 
+                'CASCADE', 
+                'CASCADE'
+            );
+
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk-option-type-id', 'option');
+        $this->dropTable('{{%option_type}}');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    private function getTableOptions()
+    {
+        $tableOptions = null;
+        
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
+        }
+        
+        return $tableOptions;
+    }
+}
