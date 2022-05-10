3489616bee46e96c42aa52b99832b3b926611987
mihailnesterov <mhause@mail.ru>
2022-05-11 02:48:53 +0700

    add: create_icon_table migration

---
+++ migrations/m220510_191946_create_icon_table.php
@@ -0,0 +1,89 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%icon}}`.
+ */
+class m220510_191946_create_icon_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $tableOptions = $this->getTableOptions();
+
+        $tableName = '{{%icon}}';
+
+        $this->createTable($tableName, [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(255)->notNull()->unique(),
+            'status' => $this->boolean()->defaultValue(1)
+        ], $tableOptions);
+
+        $table = $this->getDb()->getSchema()->getTableSchema($tableName);
+
+        if( isset($table) ) {
+
+            $this->addCommentOnTable($tableName, 'Таблица иконок');
+
+            $comments = [
+                'id' => 'id иконки',
+                'name' => 'Имя иконки',
+                'status' => 'Статус'
+            ];
+
+            foreach ($comments as $key => $value) {
+                if( !empty($table->getColumn($key)) ) {
+                    $this->addCommentOnColumn($tableName, $key, $value);
+                }
+            }
+
+            $this->addForeignKey(
+                'fk-category-icon-id', 
+                'category', 
+                'icon_id', 
+                'icon', 
+                'id', 
+                'CASCADE', 
+                'CASCADE'
+            );
+
+            $this->addForeignKey(
+                'fk-option-type-icon-id', 
+                'option_type', 
+                'icon_id', 
+                'icon', 
+                'id', 
+                'CASCADE', 
+                'CASCADE'
+            );
+
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk-category-icon-id', 'category');
+        $this->dropForeignKey('fk-option-type-icon-id', 'option_type');
+        $this->dropTable('{{%icon}}');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    private function getTableOptions()
+    {
+        $tableOptions = null;
+        
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
+        }
+        
+        return $tableOptions;
+    }
+}
