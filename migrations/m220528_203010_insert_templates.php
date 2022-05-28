51832bd324f2ebbeace416d96edecfdd0de91b03
mihailnesterov <mhause@mail.ru>
2022-05-29 03:54:35 +0700

    add: insert_templates migration

---
+++ migrations/m220528_203010_insert_templates.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220528_203010_insert_templates
+ */
+class m220528_203010_insert_templates extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $templates = [
+            ['name' => 'СИП-дом', 'type' => 'house', 'description' => 'Шаблон для СИП-домов'],
+            ['name' => 'СИП-панель', 'type' => 'pannel', 'description' => 'Шаблон для СИП-панелей'],
+        ];
+
+        foreach($templates as $key => $template) {
+            $this->insert('{{%template}}', [
+                'name' => $template['name'],
+                'type' => $template['type'],
+                'description' => $template['description']
+            ]);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        app\models\Template::deleteAll();
+    }
+
+}
